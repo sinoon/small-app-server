58dd6aa3810c349d59bc63afedd925b20819e122
sinoon <sinoon1218@gmail.com>
2018-05-15 14:16:32 +0800

    增加反射路由

--- app/routes.js
+++ app/routes.js
@@ -3,10 +3,12 @@
 const Router = require('koa-router')
 const homeController = require('./controllers/home')
 const listController = require('./controllers/list')
+const reflectionController = require('./controllers/reflection')
 
 const router = new Router()
 router.get('/', homeController.welcome)
 router.get('/spec', homeController.showSwaggerSpec)
 router.get('/list', listController.index)
+router.all('/reflection', reflectionController.index)
 
 module.exports = router
